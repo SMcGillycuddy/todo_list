07275db332f1744cab1e8fa373bed1550467ab8e
SMcGillycuddy <seadnamcgillycuddy@gmail.com>
2020-03-23 18:03:15 +0000

    Updated 23_03_2020

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,23 @@
+#import the Django package that allows us to interact with databases 
 from django.db import models
 
-# Create your models here.
+#define the database
+class List(models.Model):
+
+	#items are text
+	item = models.CharField(max_length=200)
+	#completed is True / False
+	completed = models.BooleanField(default=False)
+
+
+	def __str__(self):
+		return self.item + " | " + str(self.completed)
+
+
+
+
+
+
+
+
+
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,18 @@
+#import the django package that handles views/requests
 from django.shortcuts import render
+from .models import List
 
-# Create your views here.
+#VIEWS
 
+#function that renders and returns the 'home' page
 def home(request):
-	return render(request, 'home.html', {})
+	all_items = List.objects.all
+	return render(request, 'home.html', {'all_items': all_items})
 
+#function that renders and returns the 'about' page
 def about(request):
-	return render(request, 'about.html', {})
+	my_name = "Seadna"
+	return render(request, 'about.html', {'name': my_name})
+
+
 
